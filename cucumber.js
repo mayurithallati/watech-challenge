32f3664e5c8888aa660e15365061722f3f5d30ca
mayurithallati <158565915+mayurithallati@users.noreply.github.com>
2024-07-03 23:04:07 +0100

    cucumber reports added (#1)

--- cucumber.js
+++ cucumber.js
@@ -1,13 +1,14 @@
-  const common = [
-    'features/**/*.feature',
-    '--require-module ts-node/register',
-    '--require steps/**/*.ts',
-    '--format progress'
-  ].join(' ');
-  
-  module.exports = {
-    default: common,
-    desktop: `${common} --tags @desktop`,
-    tablet: `${common} --tags @tablet`,
-    mobile: `${common} --tags @mobile`
-  };
+const common = [
+  'features/**/*.feature',
+  '--require-module ts-node/register',
+  '--require steps/**/*.ts',
+  '--format progress',
+  '--format json:./cucumber_report.json'
+].join(' ');
+
+module.exports = {
+  default: common,
+  desktop: `${common} --tags @desktop`,
+  tablet: `${common} --tags @tablet`,
+  mobile: `${common} --tags @mobile`
+};
